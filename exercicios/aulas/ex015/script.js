179afc5e3bac8d3b1845430f94c762c6e9f041dc
OrlandoPagani <109010052+OrlandoPagani@users.noreply.github.com>
2022-09-19 15:33:29 -0300

    criado ex 015

--- exercicios/aulas/ex015/script.js
+++ exercicios/aulas/ex015/script.js
@@ -1,30 +1,51 @@
-function carregar() {
-    let msg = document.getElementById('msg');
-    let img = document.getElementById('imagem');
+function verificar() {
     let data = new Date();
-    let hora = data.getHours();
-    let minuto = data.getMinutes();
-    console.log(data, hora);
-    msg.innerHTML = `Agora são ${hora}:${minuto} `;
-    if (hora >= 5 && hora < 12) {
-        // Bom dia
-        img.src = 'imagens/morning.png';
-        document.body.style.background = '#f1e0c3';
-        msg.innetHTML += 'da manhã.';
-    } else if (hora >= 12 && hora < 19) {
-        // Boa tarde
-        img.src = 'imagens/evening.png';
-        document.body.style.background= '#fd9c24';
-        msg.innerHTML += 'da tarde.';
-    } else {
-        // Boa noite
-        img.src = 'imagens/night.png';
-        document.body.style.background = '#1f5969';
-        if (hora < 5) {
-            msg.innerHTML += 'da madrugada.'
-        } else {
-            msg.innerHTML += 'da noite.';
-        }
-    }
-}
+    let ano = data.getFullYear();
+    let formAno = document.getElementById('txtano');
+    let res = document.getElementById('res');
 
+    if (formAno.value.length == 0 || formAno.value > ano) {
+        alert('[ERRO] Por favor, verifique os dados e tente novamente!');
+    } else {
+        let formSexo = document.getElementsByName('radsex');
+        let idade = ano - Number(formAno.value);
+        let gender = '';
+        let img = document.createElement('img');
+        img.setAttribute('id', 'foto');
+        if (formSexo[0].checked) {
+            gender = 'um <strong>Homem</strong>';
+            if (idade <= 12) {
+                // Criança
+                img.setAttribute('src', 'imagens/homem-crianca.png');
+            } else if (idade < 21) {
+                // Adolescente
+                img.setAttribute('src', 'imagens/homem-jovem.png');
+            } else if (idade < 60) {
+                // Adulto
+                img.setAttribute('src', 'imagens/homem-adulto.png');
+            } else {
+                // Idoso
+                img.setAttribute('src', 'imagens/homem-idoso.png');
+            }
+        } else if (formSexo[1].checked) {
+            gender = 'uma <strong>Mulher</strong>';
+            if (idade <= 12) {
+                // Criança
+                img.setAttribute('src', 'imagens/mulher-crianca.png');
+            } else if (idade < 21) {
+                // Adolescente
+                img.setAttribute('src', 'imagens/mulher-jovem.png');
+            } else if (idade < 60) {
+                // Adulto
+                img.setAttribute('src', 'imagens/mulher-adulto.png');
+            } else {
+                // Idoso
+                img.setAttribute('src', 'imagens/mulher-idoso.png');
+            }
+        }
+        res.innerHTML = `<p>Detectamos ${gender} com ${idade} anos de idade.</p>`;
+        res.appendChild(img);
+    }
+
+    console.log(img);
+}
