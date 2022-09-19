fc29a08d50246cd312d0332a4a89b28e88574fc2
OrlandoPagani <109010052+OrlandoPagani@users.noreply.github.com>
2022-09-19 16:26:50 -0300

    criado ex 013

--- exercicios/ex013/script.js
+++ exercicios/ex013/script.js
@@ -1,7 +1,5 @@
 function iniciar() {
-    let n1 = parseInt(prompt('Digite um número:'));
-    let n2 = parseInt(prompt('Digite outro número'));
     let res = document.querySelector('div#res');
-    let maior = n1 > n2 ? n1 : n2;
-    res.innerHTML = `<p>Analisando os valores ${n1} e ${n2}, o maior valor é <strong>${maior}</strong></p>`;
+    let data = new Date();
+    res.innerHTML = `<p>O que eu recebi do sistema foi <mark>${data}</mark></p>`
 }
