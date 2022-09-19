3ea904bcc6526f9ced3582c816e1810f7960f9a1
OrlandoPagani <109010052+OrlandoPagani@users.noreply.github.com>
2022-09-19 16:42:17 -0300

    feito ex 015

--- exercicios/ex015/script.js
+++ exercicios/ex015/script.js
@@ -1,5 +1,20 @@
 function iniciar() {
     let res = document.querySelector('div#res');
     let data = new Date();
-    res.innerHTML = `<p>O que eu recebi do sistema foi <mark>${data}</mark></p>`
+    let semana = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'];
+    let meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
+    let dia = data.getDate();
+    let mes = data.getMonth();
+    let ano = data.getFullYear();
+    let diaSemana = data.getDay();
+    let hora = data.getHours();
+    let minuto = data.getMinutes();
+    let segundo = data.getSeconds();
+    res.innerHTML = `<p>Dia: <mark>${dia}</mark></p>`;
+    res.innerHTML += `<p>Mês: <mark>${meses[mes]}</mark></p>`;
+    res.innerHTML += `<p>Ano: <mark>${ano}</mark></p>`;
+    res.innerHTML += `<p>Dia da semana: <mark>${semana[diaSemana]}</mark></p>`;
+    res.innerHTML += `<p>Hora: <mark>${hora}</mark></p>`;
+    res.innerHTML += `<p>Minutos: <mark>${minuto}</mark></p>`;
+    res.innerHTML += `<p>Segundos: <mark>${segundo}</mark></p>`;
 }
