460df3b9e0df9a02f2c636ed16b1bc76342a2c37
OrlandoPagani <109010052+OrlandoPagani@users.noreply.github.com>
2022-09-19 17:05:23 -0300

    criado ex 017

--- exercicios/ex017/script.js
+++ exercicios/ex017/script.js
@@ -1,5 +1,9 @@
 function iniciar() {
+    n = Math.floor(Math.random() * 100);
     let res = document.querySelector('div#res');
-    let data = new Date();
-    res.innerHTML = `<p>O que eu recebi do sistema foi <mark>${data}</mark></p>`
+    res.innerHTML += `<p>Acabei de pensar no número <mark>${n}</mark>!</p>`
+}
+
+function limpar() {
+    res.innerHTML = '<p>Cada vez que você aperta o botão acima, eu penso em um número entre 1 e 100.</p>'
 }
