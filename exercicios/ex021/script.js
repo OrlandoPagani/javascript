8b7335939a078b498ecfc27637fabebb26d4083a
OrlandoPagani <109010052+OrlandoPagani@users.noreply.github.com>
2022-09-20 16:43:05 -0300

    feito ex 022

--- exercicios/ex021/script.js
+++ exercicios/ex021/script.js
@@ -1,7 +1,7 @@
 function iniciar() {
     let res = document.querySelector('div#res');
     res.innerHTML += '<h2>Contando de 1 até 10</h2>'
-    for (let c = 0; c <= 10; c++) {
+    for (let c = 1; c <= 10; c++) {
         res.innerHTML += `${c} \u{1F449} `
     }
     res.innerHTML += `\u{1F3C1}`
