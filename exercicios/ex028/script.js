02d4d2a2947776d3e0ec6d7760e40a5ea83425e4
OrlandoPagani <109010052+OrlandoPagani@users.noreply.github.com>
2022-09-20 18:42:57 -0300

    feito ex 028

--- exercicios/ex028/script.js
+++ exercicios/ex028/script.js
@@ -1,18 +1,22 @@
 function iniciar() {
-    let mes = prompt('Digite o mês por extenso (ex: Setembro)').toLowerCase();
+    let num = document.querySelector('input#txtn');
+    let n = Number(num.value);
     let res = document.querySelector('div#res');
 
-    res.innerHTML = `No mês de <mark>${mes}</mark>, estamos na estação `
-
-    if (mes == 'janeiro' || mes == 'fevereiro' || mes == 'março') {
-        res.innerHTML += `<strong><mark>VERÃO</mark></strong>`
-    } else if (mes == 'abril' || mes == 'maio' || mes == 'junho') {
-        res.innerHTML += `<strong><mark>OUTONO</mark></strong>`        
-    } else if (mes == 'julho' || mes == 'agosto' || mes == 'setembro') {
-        res.innerHTML += `<strong><mark>INVERNO</mark></strong>`
-    } else if (mes == 'outubro' || mes == 'novembro' || mes == 'dezembro') {
-        res.innerHTML += `<strong><mark>PRIMAVERA</mark></strong>`       
+    let fat = 1;
+    
+    if (n <= 21 && n > 0) {        
+        res.innerHTML += `<h2>Calculando ${n}!</h2>`;
+        for (let c = n; c >= 1; c--) {
+            fat *= c;
+            if (c != 1) {
+                res.innerHTML += `${c} x `;
+            } else {
+                res.innerHTML += `${c} = `
+            }
+        }
+        res.innerHTML += `<strong>${fat}</strong>`
     } else {
-        res.innerHTML = 'Desculpe. Opção inválida...'
+        alert('Por favor, utilize um número entre 1 e 21')
     }
 }
