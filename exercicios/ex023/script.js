9176d5a4d2948ccae3253f7e12e11167bc3b4c28
OrlandoPagani <109010052+OrlandoPagani@users.noreply.github.com>
2022-09-20 16:47:28 -0300

    feito ex 023 e 024

--- exercicios/ex023/script.js
+++ exercicios/ex023/script.js
@@ -1,18 +1,10 @@
 function iniciar() {
-    let mes = prompt('Digite o mês por extenso (ex: Setembro)').toLowerCase();
     let res = document.querySelector('div#res');
-
-    res.innerHTML = `No mês de <mark>${mes}</mark>, estamos na estação `
-
-    if (mes == 'janeiro' || mes == 'fevereiro' || mes == 'março') {
-        res.innerHTML += `<strong><mark>VERÃO</mark></strong>`
-    } else if (mes == 'abril' || mes == 'maio' || mes == 'junho') {
-        res.innerHTML += `<strong><mark>OUTONO</mark></strong>`        
-    } else if (mes == 'julho' || mes == 'agosto' || mes == 'setembro') {
-        res.innerHTML += `<strong><mark>INVERNO</mark></strong>`
-    } else if (mes == 'outubro' || mes == 'novembro' || mes == 'dezembro') {
-        res.innerHTML += `<strong><mark>PRIMAVERA</mark></strong>`       
-    } else {
-        res.innerHTML = 'Desculpe. Opção inválida...'
+    res.innerHTML += '<h2>Contando de 1 até 10</h2>'
+    for (let c = 1; c <= 10; c++) {
+        if (c % 2 == 0) {
+            res.innerHTML += `${c} \u{1F449} `
+        }
     }
+    res.innerHTML += `\u{1F3C1}`
 }
