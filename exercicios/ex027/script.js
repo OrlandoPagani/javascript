0598514791f7b24b0e3bca49e429164572095206
OrlandoPagani <109010052+OrlandoPagani@users.noreply.github.com>
2022-09-20 18:30:07 -0300

    feito ex 027

--- exercicios/ex027/script.js
+++ exercicios/ex027/script.js
@@ -1,18 +1,11 @@
 function iniciar() {
-    let mes = prompt('Digite o mês por extenso (ex: Setembro)').toLowerCase();
+    let num = document.querySelector('input#txtn');
+    let n = Number(num.value);
     let res = document.querySelector('div#res');
 
-    res.innerHTML = `No mês de <mark>${mes}</mark>, estamos na estação `
+    res.innerHTML = `<h2>Tabuada de ${n}</h2>`;
 
-    if (mes == 'janeiro' || mes == 'fevereiro' || mes == 'março') {
-        res.innerHTML += `<strong><mark>VERÃO</mark></strong>`
-    } else if (mes == 'abril' || mes == 'maio' || mes == 'junho') {
-        res.innerHTML += `<strong><mark>OUTONO</mark></strong>`        
-    } else if (mes == 'julho' || mes == 'agosto' || mes == 'setembro') {
-        res.innerHTML += `<strong><mark>INVERNO</mark></strong>`
-    } else if (mes == 'outubro' || mes == 'novembro' || mes == 'dezembro') {
-        res.innerHTML += `<strong><mark>PRIMAVERA</mark></strong>`       
-    } else {
-        res.innerHTML = 'Desculpe. Opção inválida...'
+    for (let c = 1; c <= 10; c++) {
+        res.innerHTML += `${n} x ${c} = <strong>${n * c}</strong><br>`
     }
 }
